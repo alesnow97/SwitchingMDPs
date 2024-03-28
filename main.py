60da597cbfbbd2ebe9b240a58970e65a174719ae
Alessio <aleruss97@gmail.com>
2024-03-28 09:42:59 +0100

    modified main

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     run_oracle = True
     run_optimistic = True
 
-    num_states = 4
+    num_states = 5
     num_actions = 6
     num_observations = 12
     num_experiments = 2
